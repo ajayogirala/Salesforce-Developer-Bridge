87ed9a7bac91a64c2efb0b979b7e9c5b8839401e
Ajay Ogirala <129488363+ajayogirala@users.noreply.github.com>
2026-08-01 03:00:06 -0700

    Update libraryHome.js

--- Sprint-04/LWC/libraryHome/libraryHome.js
+++ Sprint-04/LWC/libraryHome/libraryHome.js
@@ -1,1 +1,35 @@
+import { LightningElement } from 'lwc';
 
+export default class LibraryHome extends LightningElement {
+
+    librarianName = 'Ajay';
+
+    todayDate = new Date().toLocaleDateString();
+
+    libraryName = 'Central Library';
+
+    department = 'IT';
+
+    books = 500;
+
+    issuedBooks = 120;
+
+    members = 350;
+
+    status = 'Available';
+
+    message = '';
+
+    showMessage() {
+
+        this.message = 'Welcome to Salesforce Library Management';
+
+    }
+
+    issueBook() {
+
+        this.status = 'Issued';
+
+    }
+
+}
