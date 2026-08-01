232485b90991a49defa39a05712384e5e5f978d4
Ajay Ogirala <129488363+ajayogirala@users.noreply.github.com>
2026-08-01 01:59:00 -0700

    Update bookParent.js

--- Sprint-02/LWC-Communication/BookParent/bookParent.js
+++ Sprint-02/LWC-Communication/BookParent/bookParent.js
@@ -1,1 +1,26 @@
 
+import { LightningElement, api } from 'lwc';
+
+export default class BookChild extends LightningElement {
+
+    @api recordId;
+
+    sendMessage() {
+
+        const event = new CustomEvent(
+
+            'showmessage',
+
+            {
+
+                detail: 'Book Issued Successfully'
+
+            }
+
+        );
+
+        this.dispatchEvent(event);
+
+    }
+
+}
