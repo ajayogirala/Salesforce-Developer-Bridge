e9c3ce09cfbbf145f412552ea3f052f8321ce1ee
Ajay Ogirala <129488363+ajayogirala@users.noreply.github.com>
2026-08-01 01:54:58 -0700

    Add message handling in AvailableBooks component

--- Sprint-02/LWC-Communication/availableBooks/availableBooks.js
+++ Sprint-02/LWC-Communication/availableBooks/availableBooks.js
@@ -1,1 +1,15 @@
+import { LightningElement } from 'lwc';
 
+export default class AvailableBooks extends LightningElement {
+
+    recordId = '';
+
+    message = 'Waiting for Child Message';
+
+    handleMessage(event) {
+
+        this.message = event.detail;
+
+    }
+
+}
