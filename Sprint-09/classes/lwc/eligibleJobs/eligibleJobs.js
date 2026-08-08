c472af6f3616a16900870ff714548dc4568ab0c0
Ajay Ogirala <129488363+ajayogirala@users.noreply.github.com>
2026-08-07 23:10:46 -0700

    Implement eligible jobs component with data fetching

--- Sprint-09/classes/lwc/eligibleJobs/eligibleJobs.js
+++ Sprint-09/classes/lwc/eligibleJobs/eligibleJobs.js
@@ -1,1 +1,58 @@
+import { LightningElement, api, wire } from 'lwc';
 
+import getEligibleJobs
+    from '@salesforce/apex/ApplicationController.getEligibleJobs';
+
+export default class EligibleJobs extends LightningElement {
+
+    @api recordId;
+
+    jobs = [];
+    error;
+    isLoading = true;
+
+
+    @wire(getEligibleJobs, { studentId: '$recordId' })
+    wiredJobs({ data, error }) {
+
+        this.isLoading = false;
+
+        if (data) {
+
+            this.jobs = data;
+            this.error = undefined;
+
+        } else if (error) {
+
+            this.jobs = [];
+            this.error = error;
+
+            console.error(error);
+        }
+    }
+
+
+    get showJobs() {
+
+        return !this.isLoading &&
+               !this.error &&
+               this.jobs.length > 0;
+    }
+
+
+    get showEmpty() {
+
+        return !this.isLoading &&
+               !this.error &&
+               this.jobs.length === 0;
+    }
+
+
+    handleApply(event) {
+
+        console.log(
+            'Apply event received:',
+            event.detail
+        );
+    }
+}
